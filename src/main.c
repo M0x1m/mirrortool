cb2cecdd09e2ad4b90f04876c20c444a20f03f35
M0x1m <max9005574181@mail.ru>
2022-10-07 15:17:43 +0300

    Add the total amount of proxied bytes on closing connection

--- src/main.c
+++ src/main.c
@@ -12,18 +12,13 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <sys/ioctl.h>
-#include <termios.h>
-#include <signal.h>
 
-unsigned short COLS;
-unsigned short LINES;
 unsigned short CLIE_PORT;
 unsigned short SERV_PORT;
 struct in_addr MIRR_IP;
 char* MIRR_ADDR;
 int BIND_INTFC;
 bool ROUTE_ADDR = 0;
-struct termios termsets;
 
 struct in_addr*
 gethost(char* addr)
@@ -35,10 +30,15 @@ gethost(char* addr)
   return (struct in_addr*)reth->h_addr_list[0];
 }
 
-void*
-client_handler_thr_func(void* clientfd)
+struct client {
+  int fd;
+  struct sockaddr_in addr;
+};
+
+void
+client_handler_thr_func(struct client *client_p)
 {
-  int cfd = (int)(long long)clientfd;
+  struct client client = *client_p;
   int msock = socket(AF_INET, SOCK_STREAM, 0);
   struct sockaddr_in maddr = {
 	.sin_family = AF_INET,
@@ -47,16 +47,16 @@ client_handler_thr_func(void* clientfd)
   if(ROUTE_ADDR){
 	struct in_addr* maddrs = gethost(MIRR_ADDR);
 	if(maddrs == NULL){
-	  close(cfd);
+	  close(client.fd);
 	  close(msock);
-	  return NULL;
+	  return;
 	}
 	maddr.sin_addr = *maddrs;
   }
   else maddr.sin_addr = MIRR_IP;
   struct pollfd fds[] = {
 	{
-	  .fd = cfd,
+	  .fd = client.fd,
 	  .events = POLLIN | POLLRDHUP
 	},
 	{
@@ -64,23 +64,31 @@ client_handler_thr_func(void* clientfd)
 	  .events = POLLIN | POLLRDHUP
 	}
   };
-  connect(msock, (struct sockaddr*)&maddr, (socklen_t)sizeof(maddr));
+  if (connect(msock, (struct sockaddr*)&maddr, (socklen_t)sizeof(maddr)) < 0) {
+    close(client.fd);
+  }
+  uint64_t ttl_bytes = 0;
   while (1) {
 	poll(fds, 2, -1);
 	for (int i = 0; i < 2; i++) {
 	  if ((fds[i].revents & (POLLRDHUP | POLLHUP | POLLERR)) != 0) {
-		close(cfd);
+		close(client.fd);
 		close(msock);
-		return NULL;
+        printf("Total amount of proxied bytes on connection %s:%d[fd: %d]: %zu\n",
+               inet_ntoa(client.addr.sin_addr), ntohs(client.addr.sin_port),
+               client.fd, ttl_bytes);
+		return;
 	  }
 	}
 	for (int i = 0; i < 2; i++) {
 	  if ((fds[i].revents & POLLIN) == 1) {
-		int cap;
+		int cap = 0;
 		ioctl(fds[i].fd, FIONREAD, &cap);
-	    char buf[cap];
+	    char* buf = malloc(cap);
 		int cnt = read(fds[i].fd, buf, cap);
 		write(fds[!i].fd, buf, cnt);
+        ttl_bytes += cnt;
+        free(buf);
 	  }
 	}
   }
@@ -134,78 +142,6 @@ usage(const char* progname,
   exit(exitcode);
 }
 
-void
-thread_stop_handler(int sig)
-{
-  if (sig == SIGUSR1) {
-	ioctl(0, TCSETS, &termsets);
-	pthread_exit(NULL);
-  }
-}
-
-void screen_resz_handler(int sig)
-{
-  if (sig == SIGWINCH) {
-	struct winsize win;
-	ioctl(0, TIOCGWINSZ, &win);
-	LINES = win.ws_row;
-	COLS = win.ws_col;
-  }
-}
-
-struct main_thread_ctx{
-  int ssockfd;
-  char **argv;
-};
-
-void*
-main_thr(void* arg)
-{
-  int ssocket = ((struct main_thread_ctx*)arg) -> ssockfd;
-  char** argv = ((struct main_thread_ctx*)arg) -> argv;
-  struct sockaddr_in saddr = {
-	.sin_family = AF_INET,
-	.sin_port = CLIE_PORT,
-	.sin_addr = (struct in_addr){.s_addr = BIND_INTFC}
-  };
-  if (bind(ssocket, (struct sockaddr*)&saddr, sizeof(saddr)) < 0) {
-	fprintf(stderr, "\x1b[s\x1b[G%s: bind failed: %s\x1b[K\n\x1b[u", argv[0], strerror(errno));
-	ioctl(0, TCSETS, &termsets);
-	putc(10, stdout);
-	exit(1);
-  }
-  if (listen(ssocket, 10) < 0) {
-	fprintf(stderr, "\x1b[s\x1b[G%s: listen failed: %s\x1b[K\n\x1b[u", argv[0], strerror(errno));
-	ioctl(0, TCSETS, &termsets);
-	putc(10, stdout);
-	exit(1);
-  }
-  {
-	  int opt = 1;
-	  setsockopt(ssocket, SOL_SOCKET, SO_REUSEADDR, &opt, 4);
-	  setsockopt(ssocket, SOL_SOCKET, SO_REUSEPORT, &opt, 4);
-  }
-  while (1) {
-	int csock;
-	struct sockaddr_in caddr;
-	socklen_t caddrl = sizeof(caddr);
-	if ((csock = accept(ssocket, (struct sockaddr*)&caddr, &caddrl)) < 0) {
-	  fprintf(stderr, "\x1b[s\x1b[G%s: accept failed: %s\x1b[K\n\x1b[u", argv[0], strerror(errno));
-	  fflush(stderr);
-	  sleep(300);
-	  continue;
-	}
-	printf("\x1b[s\x1b[G%s: accepted connection[fd: %d] from: %s:%d\x1b[K\n\x1b[u", argv[0],
-			csock, inet_ntoa(caddr.sin_addr), ntohs(caddr.sin_port));
-	printf("\x1b[%d;1H\x1b[7m\x1b[2KControls: stop(C-c)\x1b[00m", LINES);
-	fflush(stdout);
-	pthread_t cthr;
-	pthread_create(&cthr, NULL, client_handler_thr_func, (void*)(long long)csock);
-	pthread_detach(cthr);
-  }
-  return NULL;
-}
-
 int
 main(int argc, char* argv[])
 {
@@ -243,55 +179,55 @@ main(int argc, char* argv[])
 		  CLIE_PORT = htons(atoi(opt));
 		  break;
 		default:
-		  fprintf(stderr, "%s: %s not implemented yet\n", argv[0], argv[i]);
+		  fprintf(stderr, "%s is not implemented yet\n", argv[i]);
 		  exit(1);
 		}
 	  } else {
-		fprintf(stderr, "%s: unknown argument %s\n", argv[0], argv[i]);
+		fprintf(stderr, "Unknown argument %s\n", argv[i]);
 		exit(1);
 	  }
 	}
   }
-  screen_resz_handler(SIGWINCH);
   if (!ROUTE_ADDR) printf("Mirroring ip: %s\n", inet_ntoa(MIRR_IP));
   else printf("Mirroring addr: %s\n", MIRR_ADDR);
   if (SERV_PORT == CLIE_PORT) printf("Mirroring port: %d\n", ntohs(CLIE_PORT));
   else printf("Ports: User->External Server: %d->%d\n", ntohs(CLIE_PORT), ntohs(SERV_PORT));
-  pthread_t main_t;
   int ssocket = socket(AF_INET, SOCK_STREAM, 0);
-  struct main_thread_ctx mctx = {
-	.ssockfd = ssocket,
-	.argv = argv
+  struct sockaddr_in saddr = {
+	.sin_family = AF_INET,
+	.sin_port = CLIE_PORT,
+	.sin_addr = (struct in_addr){.s_addr = BIND_INTFC}
   };
-  pthread_create(&main_t, NULL, main_thr, &mctx);
-  ioctl(0, TCGETS, &termsets);
-  {
-	ioctl(0, TCSETS, &(struct termios){
-		.c_iflag = ~(IGNBRK | BRKINT | PARMRK | ISTRIP
-					 | IXON),
-		.c_lflag = ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN),
-		.c_cc[VMIN] = 1
-	});
+  if (bind(ssocket, (struct sockaddr*)&saddr, sizeof(saddr)) < 0) {
+	fprintf(stderr, "Bind failed: %s\n", strerror(errno));
+	exit(1);
+  }
+  if (listen(ssocket, 10) < 0) {
+	fprintf(stderr, "Listen failed: %s\n", strerror(errno));
+	exit(1);
   }
   {
-	struct sigaction stopact = {
-	  .sa_handler = thread_stop_handler
-	};
-	sigaction(SIGUSR1, &stopact, NULL);
-	struct sigaction winact = {
-	  .sa_handler = screen_resz_handler
-	};
-	sigaction(SIGWINCH, &winact, NULL);
+    int opt = 1;
+    setsockopt(ssocket, SOL_SOCKET, SO_REUSEADDR, &opt, 4);
+    setsockopt(ssocket, SOL_SOCKET, SO_REUSEPORT, &opt, 4);
   }
-  printf("\x1b[%d;1H\x1b[7m\x1b[2KControls: stop(C-c)\x1b[00m", LINES);
-  fflush(stdout);
-  int key = 0;
   while (1) {
-	read(0, &key, 4);
-	if (key == 3) break;
+	int csock;
+	struct sockaddr_in caddr;
+	socklen_t caddrl = sizeof(caddr);
+	if ((csock = accept(ssocket, (struct sockaddr*)&caddr, &caddrl)) < 0) {
+	  fprintf(stderr, "Accept failed: %s\n", strerror(errno));
+	  fflush(stderr);
+	  sleep(30);
+	  continue;
+	}
+	printf("Accepted connection[fd: %d] from: %s:%d\n",
+           csock, inet_ntoa(caddr.sin_addr), ntohs(caddr.sin_port));
+	pthread_t cthr;
+	pthread_create(&cthr, NULL, (void*(*)(void*))client_handler_thr_func, (void*)&(struct client){
+        .fd = csock,
+        .addr = caddr});
+	pthread_detach(cthr);
   }
-  ioctl(0, TCSETS, &termsets);
-  putc(10, stdout);
-  pthread_kill(main_t, SIGUSR1);
   close(ssocket);
 }
